e012b3ae388607118695025703ed5d66799ff9af
Nicolás Órdenes <nicordenes@gmail.com>
2022-08-27 13:23:24 -0400

    feat: add guiController module

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ const gameBoard = (() => {
     addMove,
   };
 })();
+
+const guiController = (() => {
+  const board = gameBoard.board;
+  const container = document.getElementById("board");
+
+  const setUp = () => {
+    for (let index = 0; index < board.length; index++) {
+      const element = document.createElement("div");
+      container.appendChild(element);
+    }
+  };
+
+  return {
+    setUp,
+  };
+})();
+
+window.onload = () => {
+  guiController.setUp();
+};
