91d6a4b737af63da19d2d529594ffd956396d5a3
Nicolás Órdenes <nicordenes@gmail.com>
2022-08-27 13:12:59 -0400

    feat: implement gameBoard addMove method

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const gameBoard = (() => {
   const board = [null, null, null, null, null, null, null, null, null];
 
+  const addMove = (index, char) => {
+    board[index] = char;
+  };
+
   return {
     board,
+    addMove,
   };
 })();
